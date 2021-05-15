50ea3f6d299f5edbb303ac0171d5c18faf44980c
Johnson Liu <liu_johnson@yahoo.com>
2021-05-14 20:52:26 -0400

    change to sort by dates only.

--- php_MyNotes/my_notes_query.php
+++ php_MyNotes/my_notes_query.php
@@ -12,7 +12,7 @@
         }
     }
     
-    $sql2 = "SELECT a.`id`, a.`title`, a.`type_id`, b.`name` AS 'type_name', a.`update_date`, a.`content` FROM `my_notes` a, `my_notes_type` b WHERE a.`type_id` = b.`id` ORDER BY a.`type_id` ASC, a.`update_date` ASC;";
+    $sql2 = "SELECT a.`id`, a.`title`, a.`type_id`, b.`name` AS 'type_name', a.`update_date`, a.`content` FROM `my_notes` a, `my_notes_type` b WHERE a.`type_id` = b.`id` ORDER BY a.`update_date` ASC;";
     $resultArray2 = array();
     if ($result = mysqli_query($con, $sql2)) {
         $tempArray = array();
